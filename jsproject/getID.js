72ec94cddb0eb7739538f2a04cbfc8a5b51b412d
xili1342 <lucaliu217@outlook.com>
2022-07-25 23:14:01 +0800

    黑喵网站

---
+++ jsproject/getID.js
@@ -0,0 +1,15 @@
+function getID(obj) {
+    return obj.id
+    //return id;
+}
+
+
+
+function myFunction(new_src){
+    var player =  document.getElementById('b-player');
+    var id_content = getID(obj)
+    document.getElementById('b-player').setAttribute('src', id_content)
+    alert(id_content)
+    //document.getElementById('b-player').innerHTML =
+}
+
